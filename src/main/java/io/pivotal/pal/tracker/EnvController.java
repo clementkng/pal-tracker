58c7fcbc69fb04b575bcb1e7dc5b3aeadbab7f17
PCF Security Enablement <pcf-security-enablement@pivotal.io>
2018-06-07 16:50:16 -0700

    Finished Lab 2

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,36 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+    private String port;
+    private String memoryLimit;
+    private String cfInstanceIndex;
+    private String cfInstanceAddr;
+
+    public EnvController(@Value("${PORT:8080}") String port,
+                         @Value("${MEMORY_LIMIT:1000m}") String memoryLimit,
+                         @Value("${CF_INSTANCE_INDEX:0}") String cfInstanceIndex,
+                         @Value("${CF_INSTANCE_ADDR:127.0.0.1}") String cfInstanceAddr) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+        return env;
+    }
+}
